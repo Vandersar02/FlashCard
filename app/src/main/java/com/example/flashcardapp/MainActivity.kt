a49169b56589285250b3611b496a33aa67ec0cac
Vandersa02 <leejvan02@gmail.com>
2024-03-14 13:23:39 -0400

    Inital commit

--- app/src/main/java/com/example/flashcardapp/MainActivity.kt
+++ app/src/main/java/com/example/flashcardapp/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.flashcardapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,23 +14,52 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
+        val parentLayout = findViewById<RelativeLayout>(R.id.parent_layout)
+
         val flashcardAnswer1 = findViewById<TextView>(R.id.flashcard_answer1)
         val flashcardAnswer2 = findViewById<TextView>(R.id.flashcard_answer2)
         val flashcardAnswer3 = findViewById<TextView>(R.id.flashcard_answer3)
 
+        val eyesOffFilled = findViewById<ImageView>(R.id.eyes_off_filled)
+        val eyesFilled = findViewById<ImageView>(R.id.eyes_filled)
+
 
         flashcardAnswer1.setOnClickListener {
-            findViewById<View>(R.id.flashcard_answer1).setBackgroundColor(getResources().getColor(R.color.red, null))
-            findViewById<View>(R.id.flashcard_answer3).setBackgroundColor(getResources().getColor(R.color.green, null))
+            flashcardAnswer1.setBackgroundColor(getResources().getColor(R.color.red, null))
+            flashcardAnswer3.setBackgroundColor(getResources().getColor(R.color.green, null))
         }
 
         flashcardAnswer2.setOnClickListener {
-            findViewById<View>(R.id.flashcard_answer2).setBackgroundColor(getResources().getColor(R.color.red, null))
-            findViewById<View>(R.id.flashcard_answer3).setBackgroundColor(getResources().getColor(R.color.green, null))
+            flashcardAnswer2.setBackgroundColor(getResources().getColor(R.color.red, null))
+            flashcardAnswer3.setBackgroundColor(getResources().getColor(R.color.green, null))
         }
 
         flashcardAnswer3.setOnClickListener {
-            findViewById<View>(R.id.flashcard_answer3).setBackgroundColor(getResources().getColor(R.color.green, null))
+            flashcardAnswer3.setBackgroundColor(getResources().getColor(R.color.green, null))
         }
+
+        eyesOffFilled.setOnClickListener {
+            eyesOffFilled.visibility = View.INVISIBLE
+            eyesFilled.visibility = View.VISIBLE
+            flashcardAnswer1.visibility = View.GONE
+            flashcardAnswer2.visibility = View.GONE
+            flashcardAnswer3.visibility = View.GONE
+        }
+
+        eyesFilled.setOnClickListener {
+            eyesOffFilled.visibility = View.VISIBLE
+            eyesFilled.visibility = View.INVISIBLE
+            flashcardAnswer1.visibility = View.VISIBLE
+            flashcardAnswer2.visibility = View.VISIBLE
+            flashcardAnswer3.visibility = View.VISIBLE
+        }
+
+        parentLayout.setOnClickListener(View.OnClickListener { // Reset colors of the TextViews
+            flashcardAnswer1.setBackgroundColor(getResources().getColor(R.color.grown))
+            flashcardAnswer2.setBackgroundColor(getResources().getColor(R.color.grown))
+            flashcardAnswer3.setBackgroundColor(getResources().getColor(R.color.grown))
+        })
+
     }
+
 }
